bacdb76791ad3dfc4f7a71feb54e5c3e68f25358
superlou <lousimons@gmail.com>
2017-05-07 13:35:59 -0400

    Modified to determine path

--- service_clock.py
+++ service_clock.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+import os
 import socket
 import datetime
 from time import sleep
@@ -6,7 +8,9 @@ def service():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while 1:
         text = datetime.datetime.now().strftime('%-I:%M')
-        sock.sendto('next-beamer/clock/set:{}'.format(text), ('127.0.0.1', 4444))
+        folder = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+        msg = '{}/clock/set:{}'.format(folder, text)
+        sock.sendto(msg.encode(), ('127.0.0.1', 4444))
         sleep(1)
 
 
