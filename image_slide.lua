828fd3c34dadfe0805e46cfcfbea8433594802b6
superlou <lousimons@gmail.com>
2018-05-22 20:45:12 -0400

    Added image fades

--- image_slide.lua
+++ image_slide.lua
@@ -25,8 +25,21 @@ end
 function ImageSlide:draw()
   self.super:tick()
   local padding = self.padding
+  local fade_duration = 0.2
+  local time = self.super.active_time
+
+  if time < fade_duration then
+    alpha = math.min(time / fade_duration, 1.0)
+  elseif time > (self.duration - fade_duration) then
+    alpha = math.max((self.duration - time) / fade_duration, 0.0)
+  else
+    alpha = 1.0
+  end
+
+
   self.image:draw(self.width * padding, self.height * padding,
-                  self.width * (1 - padding), self.height * (1 - padding))
+                  self.width * (1 - padding), self.height * (1 - padding),
+                  alpha)
 end
 
 function ImageSlide:reset()
