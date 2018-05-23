e9ad84918b43e21ba3ae49844b3e47693fdd0701
superlou <lousimons@gmail.com>
2018-05-22 20:11:45 -0400

    Added startup message with node name

--- node.lua
+++ node.lua
@@ -4,6 +4,13 @@ Clock = require 'clock'
 
 gl.setup(1600, 900)
 
+local name = sys.get_env("NAME")
+if name then
+  print("Running with name " .. name)
+else
+  print("Running with name not set")
+end
+
 local video = util.videoplayer("loop.m4v", {["loop"] = true;})
 local slide_manager = SlideManager(0, 0, WIDTH, 800, 'data_slides.json')
 local ticker = Ticker("data_ticker.json", 0, 800, WIDTH, 100)
